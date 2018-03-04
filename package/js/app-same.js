95554265257422b92ba84bf881a3a9d6824f2d4e
timiway <cravenlal@163.com>
2018-03-04 16:43:08 +0800

    aaa

--- package/js/app-same.js
+++ package/js/app-same.js
@@ -11,8 +11,18 @@ $(function(){
   });
   $(".same-slide").on("touchstart", function(e) {
     if ($(this).position().left==170) {
-        $(".same-menu").click();
+      $(".same-menu").click();
+      $(".same-sider").addClass("container");
+      $(".same-list-side a").removeClass("selected");
         e.preventDefault();
+    }
+  });
+  $(".same-list-side a").click(function(){
+    $(this).addClass("selected").siblings().removeClass("selected");
+    var _m = $($(this).attr("data-submenu"));
+    if (_m.size()) {
+      _m.css("marginTop", (_m.parent().height()-_m.height())/2).show().siblings().hide();
+      $(".same-sider").removeClass("container");
     }
   });
   //
@@ -20,6 +30,10 @@ $(function(){
     var _icon = $(this).children(".glyphicon").toggleClass("glyphicon-chevron-down");
     $($(this).attr("href")).stop().animate({height:_icon.is(".glyphicon-chevron-down")?"100%":"4em"});
     e.preventDefault();
+  });
+  //
+  $(".post-ticket .btn-group:not(.muted)").click(function(){
+    $(this).toggleClass("active").find(".btn:last").text($(this).is(".active")?"已选":"选择");
   });
   //
   $("#link_frame").height($(window).height()-($(".same-media").is(":visible")?$(".same-media").height()+$(".same-bar").height():0));
